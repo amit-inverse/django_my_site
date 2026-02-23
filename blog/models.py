7ed10ed6ca27bf47fc6ad13d0831ef2657a56dd4
Amit Kumar Das <amit.inverse@gmail.com>
2026-02-23 21:28:00 +0530

    Tag model many-to-many

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,13 @@ from django.core.validators import MinLengthValidator
 # Create your models here.
 
 
+class Tag(models.Model):
+    caption = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.caption
+
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -21,6 +28,7 @@ class Post(models.Model):
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='posts')
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.title
