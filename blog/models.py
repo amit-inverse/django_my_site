66736713b4f2738e0ee0d4b2172b2a469cf18c7e
Amit Kumar Das <amit.inverse@gmail.com>
2026-02-23 21:26:10 +0530

    Author model and one-to-many

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,15 @@ from django.core.validators import MinLengthValidator
 # Create your models here.
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     excerpt = models.CharField(max_length=500)
@@ -11,6 +20,7 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='posts')
 
     def __str__(self):
         return self.title
